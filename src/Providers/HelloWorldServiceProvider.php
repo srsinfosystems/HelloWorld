27cbf68e59181e35f760f07813df098aad0f117e
anjulata gupta <anjulata@srs-infosystems.com>
2019-02-15 17:36:52 +0530

    provider change

--- src/Providers/HelloWorldServiceProvider.php
+++ src/Providers/HelloWorldServiceProvider.php
@@ -1,27 +1,20 @@
 <?php
- 
 namespace HelloWorld\Providers;
-  
 
-use Plenty\Plugin\RouteServiceProvider;
-use Plenty\Plugin\Routing\Router;
- 
-class HelloWorldServiceProvider extends RouteServiceProvider
+use Plenty\Plugin\ServiceProvider;
+
+/**
+ * Class HelloWorldServiceProvider
+ * @package HelloWorld\Providers
+ */
+class HelloWorldServiceProvider extends ServiceProvider
 {
- 
-    /**
-     * Register the service provider.
-     */
- 
-    public function register()
-    {
- 		//$this->getApplication()->register(HelloWorldRouteServiceProvider::class);    
- 	}
 
-    public function map(Router $router)
-    {
- 		$router->get('hello','HelloWorld\Controllers\ContentController@sayHello');
-    }
+	/**
+	 * Register the service provider.
+	 */
+	public function register()
+	{
+		$this->getApplication()->register(HelloWorldRouteServiceProvider::class);
+	}
 }
-
-?>
