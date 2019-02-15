84c46d2a5723a277f53205e57c47ac5e22a29d1d
anjulata gupta <anjulata@srs-infosystems.com>
2019-02-15 14:37:43 +0530

    discription

---
+++ src/Providers/HelloWorldServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+ 
+namespace HelloWorld\Providers;
+  
+
+use Plenty\Plugin\RouteServiceProvider;
+use Plenty\Plugin\Routing\Router;
+ 
+class HelloWorldServiceProvider extends RouteServiceProvider
+{
+ 
+    /**
+     * Register the service provider.
+     */
+ 
+    public function register()
+    {
+ 		 $this->getApplication()->register(HelloWorldRouteServiceProvider::class);
+    }
+
+    public function map(Router $router)
+    {
+ 		$router->get('hello','HelloWorld\Controllers\ContentController@sayHello');
+    }
+}
+
+?>
