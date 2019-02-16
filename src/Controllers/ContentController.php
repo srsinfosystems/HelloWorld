3ab32c84fa973814cabe42145e1a7a2526f02a37
anjulata gupta <anjulata@srs-infosystems.com>
2019-02-16 14:51:08 +0530

    import9

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -21,6 +21,39 @@ class ContentController extends Controller
 
 	public function importProduct(Twig $twig):string
 	{
-		return $twig->render('HelloWorld::content.importProduct');
+		echo "hiii";
+		$curl = curl_init();
+
+		curl_setopt_array($curl, array(
+		  CURLOPT_URL => "http://www.brandsdistribution.com/restful/export/api/products.xml?Accept=application%2Fxml&tag_26=kids",
+		  CURLOPT_RETURNTRANSFER => true,
+		  CURLOPT_ENCODING => "",
+		  CURLOPT_MAXREDIRS => 10,
+		  CURLOPT_TIMEOUT => 30,
+		  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		  CURLOPT_CUSTOMREQUEST => "GET",
+		  CURLOPT_HTTPHEADER => array(
+		    "authorization: Basic MTg0Y2U4Y2YtMmM5ZC00ZGU4LWI0YjEtMmZkNjcxM2RmOGNkOlN1cmZlcjc2",
+		    "cache-control: no-cache",
+		    "content-type: application/xml",
+		  ),
+		));
+
+		$response = curl_exec($curl);
+		$err = curl_error($curl);
+
+		curl_close($curl);
+
+		if ($err) {
+		  echo "cURL Error #:" . $err;
+		} else {
+		  // echo $response;
+		$xml = $response; 
+		  $json = json_encode($xml); // convert the XML string to JSON
+		$array = json_decode($json,TRUE); // convert the JSON-encoded string to a PHP variable
+		print_r($array);
+		}
+		exit;
+		return $twig->render('HelloWorld::content.importProduct',$array);
 	}
 }
