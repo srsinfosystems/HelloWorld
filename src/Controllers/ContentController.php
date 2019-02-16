97959bf39f7b4d158b41a7b9beca613b7093914e
anjulata gupta <anjulata@srs-infosystems.com>
2019-02-16 15:05:44 +0530

    import12

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -21,7 +21,40 @@ class ContentController extends Controller
 
 	public function importProduct(Twig $twig):string
 	{
-		echo "hiii";exit;
-		return $twig->render('HelloWorld::content.importProduct');
+		ini_set('display_errors', 1);
+		ini_set('display_startup_errors', 1);
+		error_reporting(E_ALL);
+
+
+		$curl = curl_init();
+
+		curl_setopt_array($curl, array(
+		  CURLOPT_URL => "http://www.brandsdistribution.com/restful/export/api/products.xml?Accept=application%2Fxml",
+		  CURLOPT_RETURNTRANSFER => true,
+		  CURLOPT_ENCODING => "",
+		  CURLOPT_MAXREDIRS => 10,
+		  CURLOPT_TIMEOUT => 30,
+		  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		  CURLOPT_CUSTOMREQUEST => "GET",
+		  CURLOPT_HTTPHEADER => array(
+		    "authorization: Basic MTg0Y2U4Y2YtMmM5ZC00ZGU4LWI0YjEtMmZkNjcxM2RmOGNkOlN1cmZlcjc2",
+		    "cache-control: no-cache",
+		    "content-type: application/xml"
+		  ),
+		));
+
+		$response = curl_exec($curl);
+		$err = curl_error($curl);
+
+		curl_close($curl);
+
+		if ($err) {
+		  echo "cURL Error #:" . $err;
+		} else {
+		  	$xml = $response; 
+			$json = json_encode($xml);
+			$array = json_decode($json,TRUE); 
+		  print_r(json_encode($array));exit;
+		return $twig->render('HelloWorld::content.importProduct',$array);
 	}
 }
