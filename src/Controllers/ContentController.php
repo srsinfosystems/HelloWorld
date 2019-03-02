fa9f851f8e15479cf0243791aa82651c4115a9a3
anjulata <anjulata@srs-infosystems.com>
2019-03-02 14:56:07 +0530

    test3

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -341,7 +341,7 @@ class ContentController extends Controller
 	      echo $err;
 	      //return "cURL Error #:" . $err;
 	    } else {
-	      echo $response;
+	      //echo $response;
 	      $response = json_decode($response, TRUE);
 	      $isActive = $response['isActive'];
 	      return $isActive;
